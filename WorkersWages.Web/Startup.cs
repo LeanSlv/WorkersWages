d7594bdf8c7b9b249a9ff45196c45f13055ef12a
Соловьев Михаил Александрович <ma.solovev@aisgorod.ru>
2022-03-18 19:49:14 +0400

    Configured project

--- WorkersWages.Web/Startup.cs
+++ WorkersWages.Web/Startup.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Net.Http;
+using Yarp.ReverseProxy.Forwarder;
 
 namespace WorkersWages.Web
 {
@@ -28,10 +33,52 @@ namespace WorkersWages.Web
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = Configuration.GetValue<string>("Auth:Authority");
+                options.RequireHttpsMetadata = false;
+
+                options.ClientId = Configuration.GetValue<string>("Auth:ClientId");
+                options.ClientSecret = Configuration.GetValue<string>("Auth:ClientSecret");
+                options.ResponseType = "code";
+
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.ClaimActions.MapUniqueJsonKey("middle_name", "middle_name");
+
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+
+                options.TokenValidationParameters.NameClaimType = "name";
+                options.TokenValidationParameters.RoleClaimType = "role";
+            });
+
+            services.AddAccessTokenManagement();
+
+            services.AddHttpForwarder();
+
+            // reverse proxy config
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHttpForwarder forwarder)
         {
             if (env.IsDevelopment())
             {
@@ -50,11 +97,34 @@ namespace WorkersWages.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // Configure our own HttpMessageInvoker for outbound calls for proxy operations
+            var httpMessageInvoker = new HttpMessageInvoker(new SocketsHttpHandler()
+            {
+                UseProxy = false,
+                AllowAutoRedirect = false,
+                AutomaticDecompression = DecompressionMethods.None,
+                UseCookies = false
+            });
+
+            var authority = Configuration.GetValue<string>("Auth:Authority");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+
+                endpoints.Map("/extapi/{**catch-all}", async httpContext =>
+                {
+                    var token = await httpContext.GetUserAccessTokenAsync();
+                    httpContext.Request.Cookies = null;
+                    httpContext.Request.Headers.Add("Authorization", $"Bearer {token}");
+                    httpContext.Request.Path = new PathString("/" + httpContext.Request.RouteValues["catch-all"].ToString());
+
+                    await forwarder.SendAsync(httpContext, authority, httpMessageInvoker);
+                });
             });
 
             app.UseSpa(spa =>
