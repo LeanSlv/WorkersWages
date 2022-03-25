0c1ff8933a09c438573cafcab7392b488535294c
Соловьев Михаил Александрович <ma.solovev@aisgorod.ru>
2022-03-25 13:45:41 +0400

    Added user info context

--- WorkersWages.Web/ClientApp/src/components/NavMenu.tsx
+++ WorkersWages.Web/ClientApp/src/components/NavMenu.tsx
@@ -1,20 +1,17 @@
 ﻿import { NavLink } from 'react-router-dom';
 import { BSNav, AisNavbar, AisNavDropdown, AisNavDropdownItem } from '@ais-gorod/react-ui';
-import { useCallback, useEffect, useState } from 'react';
-import { AuthorizationUserInfoResponse, WorkersWagesWebLocalApiClient } from '../services/WorkersWagesWebLocalApiClient';
+import { useCallback, useContext } from 'react';
+import { WorkersWagesWebLocalApiClient } from '../services/WorkersWagesWebLocalApiClient';
 import { MdOutlineAccountCircle, MdOutlineExitToApp, MdOutlineLogin } from 'react-icons/md';
+import { UserInfoContext } from './UserInfoContext';
 
 const apiClient = new WorkersWagesWebLocalApiClient();
 
 export const NavMenu = () => {
-    const [userInfo, setUserInfo] = useState<AuthorizationUserInfoResponse>();
+    const userInfo = useContext(UserInfoContext);
 
     const handleLogout = useCallback(async () => {
         await apiClient.logout().then((_) => window.location.reload());
-    }, []);
-
-    useEffect(() => {
-        apiClient.userInfo().then(r => setUserInfo(r));
     }, []);
 
     return (
@@ -23,23 +20,27 @@ export const NavMenu = () => {
                 <strong>Сведения о месячной зарплате рабочих</strong>
             </AisNavbar.Brand>
             <BSNav className="me-auto">
-                <BSNav.Link as={NavLink} to="/manufactories">
-                    Цеха
-                </BSNav.Link>
-                <BSNav.Link as={NavLink} to="/users">
-                    Пользователи
-                </BSNav.Link>
-                <BSNav.Link as={NavLink} to="/wages">
-                    Заработные платы
-                </BSNav.Link>
-                <AisNavDropdown
-                    id="main-nav-expanded"
-                    title={'Справочники'}
-                >
-                    <AisNavDropdownItem to="/professions">Профессии</AisNavDropdownItem>
-                    <AisNavDropdownItem to="/salaries">Оклады</AisNavDropdownItem>
-                    <AisNavDropdownItem to="/schedules">Графики работы цехов</AisNavDropdownItem>
-                </AisNavDropdown>
+                {userInfo && (
+                    <>
+                        <BSNav.Link as={NavLink} to="/manufactories">
+                            Цеха
+                        </BSNav.Link>
+                        <BSNav.Link as={NavLink} to="/users">
+                            Пользователи
+                        </BSNav.Link>
+                        <BSNav.Link as={NavLink} to="/wages">
+                            Заработные платы
+                        </BSNav.Link>
+                        <AisNavDropdown
+                            id="main-nav-expanded"
+                            title={'Справочники'}
+                        >
+                            <AisNavDropdownItem to="/professions">Профессии</AisNavDropdownItem>
+                            <AisNavDropdownItem to="/salaries">Оклады</AisNavDropdownItem>
+                            <AisNavDropdownItem to="/schedules">Графики работы цехов</AisNavDropdownItem>
+                        </AisNavDropdown>
+                    </>
+                )}
             </BSNav>
 
             <BSNav>
