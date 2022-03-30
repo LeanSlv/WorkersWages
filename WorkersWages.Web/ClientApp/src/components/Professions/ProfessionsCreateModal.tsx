66a3968412add3eeeec15a43c6a32624bfa1de0c
Соловьев Михаил Александрович <ma.solovev@aisgorod.ru>
2022-03-30 18:05:02 +0400

    Added actions modals to professions page

---
+++ WorkersWages.Web/ClientApp/src/components/Professions/ProfessionsCreateModal.tsx
@@ -0,0 +1,30 @@
+﻿import { AisModal } from "@ais-gorod/react-ui";
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { ProfessionCreateRequest, WorkersWagesApiClient } from "../../services/WorkersWagesApiClient";
+import { ProfessionsEditForm } from './ProfessionsEditForm';
+
+interface Props {
+    onDataChanged: () => void;
+}
+
+export const ProfessionsCreateModal = (props: Props) => {
+    const history = useHistory();
+
+    const propsOnDataChanged = props.onDataChanged;
+    const handleSubmit = useCallback(async (data: ProfessionCreateRequest) => {
+        const apiClient = new WorkersWagesApiClient('/extapi');
+        await apiClient.professionsPOST(data).then((_) => {
+            history.goBack();
+            propsOnDataChanged();
+        });
+    }, [history, propsOnDataChanged]);
+
+    return (
+        <AisModal show={true} onHide={() => history.goBack()} title="Добавление профессии">
+            <ProfessionsEditForm onSubmit={handleSubmit} />
+        </AisModal>
+    );
+};
+
+ProfessionsCreateModal.displayName = 'ProfessionsCreateModal';
---
+++ WorkersWages.Web/ClientApp/src/components/Professions/ProfessionsEditForm.tsx
@@ -0,0 +1,22 @@
+﻿import { AisButton, AisForm, AisFormField, Yup } from "@ais-gorod/react-ui";
+import { ProfessionEditRequest } from "../../services/WorkersWagesApiClient";
+
+interface Props {
+    data?: ProfessionEditRequest;
+    onSubmit: (data: ProfessionEditRequest) => void;
+}
+
+const validationSchema = Yup.object().shape({
+    name: Yup.string().required()
+});
+
+export const ProfessionsEditForm = (props: Props) => {
+    return (
+        <AisForm onSubmit={props.onSubmit} validationSchema={validationSchema} initialValues={props.data}>
+            <AisFormField.Text label="Название" name="name" />
+            <AisButton type="submit">Сохранить</AisButton>
+        </AisForm>
+    );
+};
+
+ProfessionsEditForm.displayName = 'ProfessionsEditForm';
