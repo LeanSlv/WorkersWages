42a01312c5c2be87088e0d3e357fcdb63184536d
Соловьев Михаил Александрович <ma.solovev@aisgorod.ru>
2022-04-15 15:52:06 +0400

    Added authorize attribute to controllers

--- WorkersWages.API/API/Manufactories/ManufactoriesController.cs
+++ WorkersWages.API/API/Manufactories/ManufactoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace WorkersWages.API.API.Manufactories
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ManufactoriesController : ControllerBase
     {
         private readonly DataContext _dataContext;
